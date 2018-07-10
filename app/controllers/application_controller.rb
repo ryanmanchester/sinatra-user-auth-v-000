1dd6ca2b66744f86657e68ba82d02fbf90b68a37
Ryan Manchester <ryanm17@gmail.com>
2018-07-10 20:05:09 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,7 +30,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/sessions' do
-    puts params
+    @user = User.find_by(email: params["email"], password: params["password"])
     redirect '/users/home'
   end
 
